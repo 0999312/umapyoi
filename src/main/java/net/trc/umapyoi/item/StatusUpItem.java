d4362743c624fcb08c4e2b72d613ba14934559d6
unknown <617510630@qq.com>
2022-07-29 03:16:38 +0800

    精简代码

--- src/main/java/net/trc/umapyoi/item/StatusUpItem.java
+++ src/main/java/net/trc/umapyoi/item/StatusUpItem.java
@@ -7,9 +7,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.trc.umapyoi.api.UmaStatus;
+import net.trc.umapyoi.api.UmapyoiAPI;
 import net.trc.umapyoi.capability.CapabilityRegistry;
-import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
 
 public class StatusUpItem extends Item {
     private final UmaStatus status;
@@ -23,65 +22,58 @@ public class StatusUpItem extends Item {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        if(level.isClientSide())
+        if (level.isClientSide())
             return super.use(level, player, hand);
-        if (CuriosApi.getCuriosHelper().getCuriosHandler(player).isPresent()) {
-            var itemHandler = CuriosApi.getCuriosHelper().getCuriosHandler(player).orElse(null);
-            if (itemHandler.getStacksHandler("uma_soul").isPresent()) {
-                var stacksHandler = itemHandler.getStacksHandler("uma_soul").orElse(null);
-                IDynamicStackHandler stackHandler = stacksHandler.getStacks();
-
-                if (stackHandler.getSlots() > 0) {
-                    stackHandler.getStackInSlot(0).getCapability(CapabilityRegistry.UMACAP).ifPresent(cap -> {
-                        switch (status) {
-                        case SPEED: {
-                            cap.setSpeed(cap.getSpeed() + value);
-                            break;
-                        }
-                        case STAMINA: {
-                            cap.setStamina(cap.getStamina() + value);
-                            break;
-                        }
-                        case STRENGTH: {
-                            cap.setStrength(cap.getStrength() + value);
-                            break;
-                        }
-                        case MENTALITY: {
-                            cap.setMentality(cap.getMentality() + value);
-                            break;
-                        }
-                        case WISDOM: {
-                            cap.setWisdom(cap.getWisdom() + value);
-                            break;
-                        }
-                        case MAX_SPEED: {
-                            cap.setMaxSpeed(cap.getMaxSpeed() + value);
-                            break;
-                        }
-                        case MAX_STAMINA: {
-                            cap.setMaxStamina(cap.getMaxStamina() + value);
-                            break;
-                        }
-                        case MAX_STRENGTH: {
-                            cap.setMaxStrength(cap.getMaxStrength() + value);
-                            break;
-                        }
-                        case MAX_MENTALITY: {
-                            cap.setMaxMentality(cap.getMaxMentality() + value);
-                            break;
-                        }
-                        case MAX_WISDOM: {
-                            cap.setMaxWisdom(cap.getMaxWisdom() + value);
-                            break;
-                        }
-                        default:
-                            throw new IllegalArgumentException("Unexpected value: " + status);
-                        }
-                        player.getCooldowns().addCooldown(player.getItemInHand(hand).getItem(), 30);
-                        player.getItemInHand(hand).shrink(1);
-                    });
+        ItemStack uma_soul = UmapyoiAPI.getUmaSoul(player);
+        if (!uma_soul.isEmpty()) {
+            uma_soul.getCapability(CapabilityRegistry.UMACAP).ifPresent(cap -> {
+                switch (status) {
+                case SPEED: {
+                    cap.setSpeed(cap.getSpeed() + value);
+                    break;
                 }
-            }
+                case STAMINA: {
+                    cap.setStamina(cap.getStamina() + value);
+                    break;
+                }
+                case STRENGTH: {
+                    cap.setStrength(cap.getStrength() + value);
+                    break;
+                }
+                case MENTALITY: {
+                    cap.setMentality(cap.getMentality() + value);
+                    break;
+                }
+                case WISDOM: {
+                    cap.setWisdom(cap.getWisdom() + value);
+                    break;
+                }
+                case MAX_SPEED: {
+                    cap.setMaxSpeed(cap.getMaxSpeed() + value);
+                    break;
+                }
+                case MAX_STAMINA: {
+                    cap.setMaxStamina(cap.getMaxStamina() + value);
+                    break;
+                }
+                case MAX_STRENGTH: {
+                    cap.setMaxStrength(cap.getMaxStrength() + value);
+                    break;
+                }
+                case MAX_MENTALITY: {
+                    cap.setMaxMentality(cap.getMaxMentality() + value);
+                    break;
+                }
+                case MAX_WISDOM: {
+                    cap.setMaxWisdom(cap.getMaxWisdom() + value);
+                    break;
+                }
+                default:
+                    throw new IllegalArgumentException("Unexpected value: " + status);
+                }
+                player.getCooldowns().addCooldown(player.getItemInHand(hand).getItem(), 30);
+                player.getItemInHand(hand).shrink(1);
+            });
         }
         return super.use(level, player, hand);
     }
--- src/main/java/net/trc/umapyoi/item/UmaSoulItem.java
+++ src/main/java/net/trc/umapyoi/item/UmaSoulItem.java
@@ -52,32 +52,20 @@ public class UmaSoulItem extends Item {
                         .forEach(data -> items.add(UmapyoiAPI.setUmaData(getDefaultInstance(), data)));
         }
     }
-    
-    /**
-     *  用来获取待同步的`Tag`，默认实现为获取`ItemStack`本身的 tag。
-     *  注意！如果这里覆写方法没有调用 super，可能会导致`ItemStack`本身的同步失效。 
-     */
+
     @Nullable
     @Override
     public CompoundTag getShareTag(ItemStack stack) {
-        // 由于默认实现是 getTag，这里就不判空了，直接使用 stack.getOrCreateTag()
-        var result = stack.getOrCreateTag();
-        // 获取 capability 的值存入 result，这里拿 ENERGY 举例，因为我的上下文确定这里用的一定是 EnergyStorage 所以直接强转了，可以使用 instaceof INBTSerializable serializable 来判断是否可以直接 serializeNBT
+        var result = super.getShareTag(stack);
         stack.getCapability(CapabilityRegistry.UMACAP).ifPresent(cap -> {
             result.put("soul", cap.serializeNBT());
         });
         return result;
     }
 
-    /** 用来在`Tag`同步后读取的方法。 */
     @Override
     public void readShareTag(ItemStack stack, @Nullable CompoundTag nbt) {
         super.readShareTag(stack, nbt);
-        // 他不太可能是 null，但判空保平安，省得爆了再去找原因
-        if (nbt == null) {
-            return;
-        }
-        // 同样，获取 capability 的值。
         stack.getCapability(CapabilityRegistry.UMACAP).ifPresent(cap -> {
             cap.deserializeNBT(nbt.getCompound("soul"));
         });
